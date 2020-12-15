ac63ae73eefd43c019e7776818180fb2ba6b88f7
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2020-12-15 22:04:32 +0500

    chanllenge 2

--- src/index.js
+++ src/index.js
@@ -22,3 +22,17 @@ ReactDOM.render(
 </>,
    document.getElementById('root')
   );
+  // chanllenge two
+  const myname = "Zaib Un Nisa";
+  const currentdate = new Date().toLocaleDateString();
+  const currentTime = new Date().toLocaleTimeString();
+
+  ReactDOM.render(
+    <>
+    <h1>Hello, My name is {myname}.</h1>
+    <p>Current Date is: {currentdate} </p>
+    <p>Current Time is: {currentTime} </p>
+
+    </>,
+    document.getElementById('root')
+  )
