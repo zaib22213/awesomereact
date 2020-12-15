e7efdf1de75f408723c05037b95510bad783440f
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2020-12-15 22:33:53 +0500

    practice img in react

--- src/index.js
+++ src/index.js
@@ -5,34 +5,57 @@ import ReactDOM from 'react-dom';
 
 // Chanllenge one
 
-ReactDOM.render(
-<>
-<h1>Netflix</h1>
-<p>List of Netflix series</p>
-<ol>
-  <li>DARK</li>
-  <li>DANGER</li>
-  <li>CARTOON</li>
-  <li>DRAMMA</li>
-  <li>FILM</li>
-  <li>FILM</li>
+// ReactDOM.render(
+// <>
+// <h1>Netflix</h1>
+// <p>List of Netflix series</p>
+// <ol>
+//   <li>DARK</li>
+//   <li>DANGER</li>
+//   <li>CARTOON</li>
+//   <li>DRAMMA</li>
+//   <li>FILM</li>
+//   <li>FILM</li>
 
 
-</ol>
+// </ol>
+// </>,
+//    document.getElementById('root')
+//   );
+//   // chanllenge two
+//   const myname = "Zaib Un Nisa";
+//   const currentdate = new Date().toLocaleDateString();
+//   const currentTime = new Date().toLocaleTimeString();
+
+//   ReactDOM.render(
+//     <>
+//     <h1>Hello, My name is {myname}.</h1>
+//     <p>Current Date is: {currentdate} </p>
+//     <p>Current Time is: {currentTime} </p>
+
+//     </>,
+//     document.getElementById('root')
+//   )
+
+//  Practice images and anchar tag
+const name = 'zaib';
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/270/300";
+const img4 = "https://picsum.photos/300/300";
+const link = "https://www.w3schools.com/";
+
+ ReactDOM.render(
+   <>
+<h1>my name is {name}</h1>
+<img src={img1} alt="randomimage" />
+<img src={img2} alt="randomimage" />
+<img src={img3} alt="randomimage" />
+<a href={link}  target="_w3school">
+<img src={img4} alt="randomimage" />
+
+</a>
+
 </>,
-   document.getElementById('root')
-  );
-  // chanllenge two
-  const myname = "Zaib Un Nisa";
-  const currentdate = new Date().toLocaleDateString();
-  const currentTime = new Date().toLocaleTimeString();
-
-  ReactDOM.render(
-    <>
-    <h1>Hello, My name is {myname}.</h1>
-    <p>Current Date is: {currentdate} </p>
-    <p>Current Time is: {currentTime} </p>
-
-    </>,
     document.getElementById('root')
-  )
+ );
