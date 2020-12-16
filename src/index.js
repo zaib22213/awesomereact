863747626007c9e5af20155d996e0151f1a34f4a
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2020-12-16 15:27:30 +0500

    commit miniproject

--- src/index.js
+++ src/index.js
@@ -63,22 +63,42 @@ const obj = {
    justifyContent: "center"
  }
 
- ReactDOM.render(
-   <>
-<h1 style={obj}>my name is {name}</h1>
-<div style={Div_img}>
-<img src={img1} alt="randomimage" />
-<img src={img2} alt="randomimage" />
-<img src={img3} alt="randomimage" />
-<a href={link}  target="_w3school">
-<img src={img4} alt="randomimage" />
+//  ReactDOM.render(
+//    <>
+// <h1 style={obj}>my name is {name}</h1>
+// <div style={Div_img}>
+// <img src={img1} alt="randomimage" />
+// <img src={img2} alt="randomimage" />
+// <img src={img3} alt="randomimage" />
+// <a href={link}  target="_w3school">
+// <img src={img4} alt="randomimage" />
 
-</a>
-</div>
+// </a>
+// </div>
 
-</>,
-    document.getElementById('root')
- );
+// </>,
+//     document.getElementById('root')
+//  );
 
 
- 
+// mini project to show good morning and afternoon and night
+
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting = "";
+if (curDate >= 1 && curDate < 12) {
+greeting = "Good Morning";
+} else if(curDate >= 12 && curDate < 19) {
+   greeting = "Good Afternoon";
+
+} else {
+   greeting = "Good Night";
+
+}
+
+ReactDOM.render(<h1>Hello Sir, {greeting}</h1>,
+   document.getElementById('root'));
+
+
+
+
