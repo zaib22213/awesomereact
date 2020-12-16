80f77fcc792755f1c8b47c5c309efaf7b7881676
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2020-12-16 19:50:05 +0500

    commit netflix

--- src/index.js
+++ src/index.js
@@ -1,40 +1,27 @@
-// var React = require('react');
-import  React from 'react';
-// var ReactDOM = require('react-dom');
+import Cards from './Cards';
+import React from 'react';
 import ReactDOM from 'react-dom';
-// import script from './script';
-
-// Make the component of the Card
-function Card(props) {
-   return(
-       <>
-      <div className="cards">
-      <div className="card">
-         <img src={props.imgsrc}
-          alt="mypic" 
-          className="card_img"
-          />
-          <div className="card_info">
-             <span className="card_category">{props.title}</span>
-             <h3 className="card_title">{props.sname}</h3>
-             <a href={props.links} target="_blank">
-             <button>Watch Now</button>
-             </a>
-             
-          </div>
-   
-      </div>
-   
-      </div>
-      </>
-     );
-}
 
 ReactDOM.render(
    <> 
-<Card sname="Dark"
+<Cards sname="Dark"
     imgsrc="https://lh3.googleusercontent.com/proxy/wQbhn6BPNErZ4_YrbLkYxXZ4IT5rx9u7u1hP6mI8Babu183UtsHEESdPsyaNapMtEzeiUKRBcPyMNSwr9_YeosYVUw6_NXLHF22iQmzPlVl8U5iyjJNT6A"
     title="Netflix Original Series"
     links="https://www.netflix.com/pk/title/80100172" />
+    
+<Cards 
+    sname="extra curricular"
+    imgsrc="https://m.media-amazon.com/images/M/MV5BMzM2NTAzMzYtNWVlNi00NzFmLWE3MjgtMWI5NWI2MjAzZjdmXkEyXkFqcGdeQXVyODE2MzY0ODM@._V1_.jpg"
+    title="Netflix Original Series"
+    links="https://www.netflix.com/pk/title/80990668#:~:text=A%20model%20high%20school%20student,an%20interest%20in%20his%20secret."
+
+    />
+    <Cards 
+    sname="Stranger Things"
+    imgsrc="https://cdn.onebauer.media/one/media/5d18/823d/2851/e25e/3066/1778/tcTjAGX9YPCMCrnXmbgXKKtECgh.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg"
+    title="Netflix Original Series"
+    links="https://www.netflix.com/pk/title/80057281"
+
+    />
     </>
    , document.getElementById('root'));
