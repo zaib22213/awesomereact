61eded650799a62238a04e426e3425031c959767
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2020-12-20 22:33:54 +0500

    commit new

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
 import Heading from './Heading';
-import Parag from './Parag';
-import List from './List';
+import React from 'react';
 
-function App(){
-    return(
-        <>
-        <Heading />
-        <Parag />
-        <List />
-        </>
+// import Parag from './Parag';
+// import List from './List';
 
-    );
-}
+// function App(){
+//     return(
+//         <>
+//         <Heading />
+//         <Parag />
+//         <List />
+//         </>
+
+//     );
+// }
 
 
 export default App;
