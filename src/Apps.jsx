5445b2dfeb58ca11408d2461854389df54ca4b3a
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2020-12-21 20:02:52 +0500

    commit 2

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
+import React from "react";
 
-const SlotM = () => {
-    let x = '😊';
-    let y = '😊';
-    let z = '😊';
-    if ((x===y) && (y===z)) {
-       return (
-           <>
-               <div>
-                   <h1> {x} {y} {y}</h1>
-                <h1>This is matching</h1>
-               </div>
-           </>
-       )
-    } else {
+const SlotM = (props) => {
+  //   let x = "😊";
+  //   let y = "😊";
+  //   let z = "🎅";
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
 
-    }
-}
+  if (x === y && y === z) {
+    return (
+      <>
+        <div className="slot_inner">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1>This is Matching</h1>
+          <hr />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="slot_inner">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1>This is Not Matching</h1>
+          <hr />
+        </div>
+      </>
+    );
+  }
+};
 
 const Apps = () => {
-return (
-<>
-<h1 className='heading_style'> 
-🎰 Welcome to <span style={{fontWeight:'bold'}}>Slot machine game</span>🎰
-</h1>
-<SlotM />
-</>)
+  return (
+    <>
+      <h1 className="heading_style">
+        🎰 Welcome to{" "}
+        <span style={{ fontWeight: "bold" }}>Slot machine game</span>🎰
+      </h1>
+      <div className="slotmachine">
+        <SlotM x="😊" y="😊" z="🎅" />
+        <SlotM x="🍎" y="😊" z="🎅" />
+        <SlotM x="😊" y="🎅" z="🍉" />
+        <SlotM x="🍉" y="🍉" z="🍉" />
+      </div>
+    </>
+  );
 };
 export default Apps;
