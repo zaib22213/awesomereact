254b9a832a2c86a8b8cd3a611da9acf4a4c4e79b
ZAIB UN NISA <63113414+zaib22213@users.noreply.github.com>
2021-01-08 21:24:12 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-// import Heading from './Heading';
-
-import React, { useState } from "react";
-
-// import React from 'react';
+import React from "react";
 
 const App = () => {
-  const state = useState();
-  let CurrTime = new Date().toLocaleTimeString();
-  const [CTime, setTime] = useState(CurrTime);
-  const UpdatedTime = () => {
-    CurrTime = new Date().toLocaleTimeString();
-    setTime(CurrTime);
-    // setCount(count + 1);
-    // console.log('Clicked' + count++);
-  };
-  return (
+  return(
     <>
       <div>
-        <h1> {CTime} </h1>
-        <button onClick={UpdatedTime} >GET TIME</button>
+        <form>
+          <h1>Hello</h1>
+        </form>
       </div>
     </>
   );
-};
-
-// import Parag from './Parag';
-// import List from './List';
-
-// function App(){
-//     return(
-//         <>
-//         <Heading />
-//         <Parag />
-//         <List />
-//         </>
-
-//     );
-// }
-
+}
 export default App;
